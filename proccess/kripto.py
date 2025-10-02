1c7d86339ba372dc97e9e4f23eb7cb0a4c1c1edf
Arya Wiratama <aryawiratama2401@gmail.com>
2025-10-02 18:18:24 +0700

    update: caesar chiper terminal

---
+++ proccess/kripto.py
@@ -0,0 +1,37 @@
+from main import caesar_decrypt, caesar_encrypt
+
+while True:
+    
+    data_input = input("Masukan kata : ")
+    jumlah_key = int(input("Masukan Key : "))
+    opsi = int(input("1.Encrypt\n2.Decrypt\nPilihan Kamu : "))
+
+    if opsi == 1:
+        hasil_encrypt = caesar_encrypt(data_input, jumlah_key)
+        print(hasil_encrypt)
+        is_decrypt = input("Apakah ingin di decrypt (y/n) : ")
+        if is_decrypt == 'y' or is_decrypt == 'Y':
+            print(caesar_decrypt(hasil_encrypt, jumlah_key))
+        else:
+            print("Input Tidak Valid")
+        
+    elif opsi == 2:
+        print(caesar_decrypt(data_input, jumlah_key))
+    else :
+        print("Input tidak valid (Masukan antara 1 dan 2)")
+        
+    print("\n--------------------------------------------------\n")
+
+    is_done = input("Apakah mau jalankan ulang program (y/n) : ")
+    if is_done == 'n' or is_done == 'N':
+        break
+    
+    
+print("\nProgram Berakhir")
+
+
+    
+
+
+
+    
